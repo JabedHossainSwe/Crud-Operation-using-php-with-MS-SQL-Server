02e2ab5c2d37dd5a65b7b511a3f863800a6792db
JabedHossainSwe <jabedhossain.swe@gmail.com>
2023-10-24 22:15:57 +0600

    sql server done

--- SQL Server/index.php
+++ SQL Server/index.php
@@ -1,18 +1,46 @@
-<?php
-$servername = "DESKTOP-8COBBVN";
-$database = "SampleDB";
-$username = "";
-$password = "";
+<!DOCTYPE html>
+<html>
 
-$connection = [
-    "Database" => $database,
-    "username" => $username,
-    "password" => $password,
-];
+<head>
+    <title>Insert Data</title>
+</head>
 
-$conn = sqlsrv_connect($servername, $connection);
-if (!$conn) {
-   die(print_r(sqlsrv_errors(),true)); 
-}else{
-    echo 'connection successful';
-}
+<body>
+    <h2>Insert Data</h2>
+    <form method="post">
+        <label>Name:</label>
+        <input type="text" name="name" required><br>
+
+        <label>Mobile:</label>
+        <input type="text" name="mobile" required><br>
+
+        <input type="submit" value="Insert">
+    </form>
+
+    <?php
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+        $name = $_POST['name'];
+        $mobile = $_POST['mobile'];
+
+
+        include 'connect_sqlsrv.php';
+
+        // Insert data into the SQL Server database
+        $sql = "INSERT INTO SampleTable (name, mobile) VALUES (?, ?)";
+        $params = array($name, $mobile);
+
+        $stmt = sqlsrv_query($conn, $sql, $params);
+
+        if ($stmt) {
+            echo "Data inserted successfully.";
+        } else {
+            echo "Error: " . print_r(sqlsrv_errors(), true);
+        }
+
+        sqlsrv_close($conn);
+    }
+    ?>
+</body>
+
+</html>
