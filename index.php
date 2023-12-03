82052a2f4332545757af4af0d0b1c10e981555e0
JabedHossainSwe <jabedhossain.swe@gmail.com>
2023-12-04 01:51:46 +0600

    Update index.php
    - Updated the CSS and JS libraries to the latest versions.
    - Added a print button to the student data table.
    - Added a function to generate PDF of the student data table.
    - Updated the Bootstrap version to 5.2.0.

--- index.php
+++ index.php
@@ -1,13 +1,3 @@
-<?php
-include './config/connect_sqlsrv.php';
-$sql = "SELECT * FROM Student";
-$result = $conn->query($sql);
-
-if ($result === false) {
-        die("Error: " . implode(" - ", $conn->errorInfo()));
-}
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -15,19 +5,27 @@ if ($result === false) {
         <meta charset="UTF-8">
         <title>Student Data</title>
 
-        <link href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css" rel="stylesheet">
-
         <!-- Bootstrap CSS -->
         <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 
         <!-- Font Awesome CSS -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
 
-        <!-- DataTables JS -->
+        <!-- DataTables CSS -->
+        <link href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css" rel="stylesheet">
+
+
+        <!-- jsPDF library -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.4.0/jspdf.umd.min.js"></script>
+
+        <!-- Your custom JS file -->
+        <script src="js/index.js"></script>
+
+        <!-- jQuery and DataTables JS -->
         <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
         <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
 
-        <!-- Bootstrap JS (if not already included in your project) -->
+        <!-- Bootstrap JS -->
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
         <!-- Your custom JS file -->
@@ -35,6 +33,15 @@ if ($result === false) {
 
         <!-- Your custom CSS file -->
         <link rel="stylesheet" href="./css/style.css">
+
+        <!-- Additional styles for hiding elements during print -->
+        <style>
+                @media print {
+                        .no-print {
+                                display: none;
+                        }
+                }
+        </style>
 </head>
 
 <body>
@@ -48,24 +55,30 @@ if ($result === false) {
                         </button>
                 </div>
 
-                <div class="print-btn">
-                        <button type="button" id="printButton" class="btn btn-secondary">
-                                Print Data
+                <div class="btn-group no-print">
+                        <button type="button" onclick="generatePDF()" class="btn btn-secondary">
+                                Download PDF
                         </button>
                 </div>
 
-                <!-- bootstrap datatable for showing student's info -->
+                <div class="btn-group no-print">
+                        <button type="button" onclick="window.print()" class="btn btn-secondary">
+                                Print
+                        </button>
+                </div>
+
+                <!-- Bootstrap datatable for showing student's info -->
                 <?php include './Table/studentTable.php'; ?>
 
                 <!-- Modal for Adding Student -->
                 <?php include './Form/addStudentForm.php'; ?>
-
         </div>
 
         <script>
                 $(document).ready(function () {
+                        // Initialize DataTable
                         var table = $('#studentTable').DataTable({
-                                "paging": true,
+                                "paging": false,
                                 "searching": true,
                                 "ordering": true,
                                 "info": true,
@@ -77,15 +90,19 @@ if ($result === false) {
                                 }
                         });
 
-                        // Handle the print button click event
-                        $('#printButton').on('click', function () {
-                                window.print();
-                        });
+                        // Function to generate PDF
+                        window.generatePDF = function () {
+                                // Create jsPDF instance
+                                const pdf = new jsPDF();
+
+                                // Add content to the PDF using autoTable
+                                pdf.autoTable({ html: '#studentTable' });
+
+                                // Save the PDF
+                                pdf.save('Student_Data.pdf');
+                        };
                 });
         </script>
-
-        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 
 </html>
